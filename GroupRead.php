ebbb305fa2fc228bc3b249e8f827ecc9cf91e328
taiga533 <kigtaiga@gmail.com>
2017-05-17 17:18:48 +0900

    SQLインジェクションに対する耐性を付与（教えてくださった○○氏、ありがとうございます。）

--- GroupRead.php
+++ GroupRead.php
@@ -2,16 +2,15 @@
 header('Content-type: application/json');
 define ('DB_USER', "hoge");
 define ('DB_PASSWORD', "hoge");
-define ('DB_DATABASE', "hoge");
-define ('DB_HOST', "hoge");
-$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
-$mysqli->set_charset("utf8");
-$sql = "SELECT marketgroups.marketGroupID,
+define ('DSN', "mysql:host=hoge;dbname=hoge;charset=utf8");
+$dbh = new PDO(DSN,DB_USER,DB_PASSWORD,array(PDO::ATTR_EMULATE_PREPARES => false));
+$stmt = $dbh->prepare("SELECT marketgroups.marketGroupID,
 				marketgroups.marketGroupName  FROM marketgroups
-		WHERE parentGroupID LIKE ".$_GET['q'];
-$result = $mysqli->query($sql);
+		WHERE parentGroupID LIKE ?");
+$stmt->bindParam(1, $_GET['q']);
+$stmt->execute();
 $json = [];
-while($row = $result->fetch_assoc()){
+while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
      $json[] = ['id'=>$row['marketGroupID'], 'text'=>$row['marketGroupName']];
 }
 echo json_encode($json);
